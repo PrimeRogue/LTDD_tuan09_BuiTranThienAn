9034f28435cab92ed6f83d142a389fd913363382
Bui Tran Thien An <buitranthienan1111@gmail.com>
2023-11-14 16:26:57 +0700

    Create UI 'Create Account' and POST user, name when clicking SignUp Button - SC01

---
+++ README.md
@@ -0,0 +1,13 @@
+# LTDD_tuan08_BuiTranThienAn
+# 1. Screen01: handle loading
+![](demos/2.PNG)
+# 2. Screen01: fetch donutDB from MockAPI using Axios
+![](demos/1.PNG)
+# 3. Screen01: handle data transfer when clicking the button 
+![](demos/3.PNG)
+# 4. Screen01: handle search
+- Return filteredData array when a result is found 
+- Return empty array when no results are found
+- Return originalData when textSearch is empty
+![](demos/4.PNG)
+
---
+++ Screen01.js
@@ -0,0 +1,159 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { CheckBox } from "react-native-elements";
+import axios from "axios";
+
+export default function Screen01() {
+  const [isChecked, setIsChecked] = useState(false);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleSignUp = async () => {
+    const user = { name, password };
+
+    try {
+      const response = await axios.post("http://localhost:3000/account", user);
+      const userCreated = response.data;
+
+      console.log("User created:", userCreated);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("./assets/user.png")}
+        style={{ width: 150, height: 150, opacity: 0.7 }}
+      ></Image>
+      <Text style={{ color: "#f69697", fontSize: 25, fontWeight: "bold" }}>
+        Create Account
+      </Text>
+      <TextInput
+        placeholder="Name"
+        style={styles.input}
+        onChangeText={setName}
+      ></TextInput>
+
+      <TextInput
+        placeholder="Password"
+        style={styles.input}
+        onChangeText={setPassword}
+      ></TextInput>
+      <CheckBox
+        checked={isChecked}
+        onPress={handleCheckboxChange}
+        title="I agree to the Term and Privacy Policy"
+        style={{ backgroundColor: "#3A434A !important" }}
+      />
+      <View
+        style={{
+          justifyContent: "center",
+          flexDirection: "row",
+          alignItems: "center",
+          width: "100%",
+          gap: 10,
+        }}
+      >
+        <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+          <Text style={{ fontSize: 18, fontWeight: "bold", color: "#fff" }}>
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ fontSize: 18, fontWeight: "bold", color: "#fff" }}>
+            Sign In
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    flexDirection: "column",
+    gap: 20,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    width: "30%",
+  },
+  selectedButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    width: "30%",
+    backgroundColor: "#F1B000",
+  },
+  itemContainer: {
+    width: "100%",
+    flexDirection: "column",
+    gap: 10,
+  },
+  item: {
+    flexDirection: "row",
+    backgroundColor: "#F4DDDD",
+    alignItems: "center",
+    borderRadius: 15,
+    padding: 5,
+    gap: 10,
+    marginTop: 30,
+  },
+  plus: {
+    width: 40,
+    height: 40,
+    backgroundColor: "yellow",
+    borderRadius: 15,
+    borderTopLeftRadius: 90,
+    position: "absolute",
+    bottom: 5,
+    right: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  input: {
+    width: "100%",
+    padding: 15,
+    borderWidth: 2,
+    borderColor: "#f69697",
+    borderRadius: 10,
+    color: "#ccc",
+    fontSize: 18,
+    fontWeight: "400",
+  },
+
+  button: {
+    padding: 15,
+    paddingLeft: 30,
+    paddingRight: 30,
+    borderWidth: 1,
+    backgroundColor: "#f69697",
+    borderRadius: 10,
+    borderColor: "#f69697",
+  },
+});
